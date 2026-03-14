94bebbf65c62300cdacbf32c1a68ce8b655af7ba
jose villegas <josevillegas13600@gmail.com>
2026-03-14 15:32:07 -0400

    configuracion de la app y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import PageHeader from './components/PageHeader.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Countries app</h1>
+  <PageHeader/>
 </template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center my-24">
+        <h1 class="text-4xl font-bold tracking-widest text-sky-700">Aplicacion de paises</h1>
+        <h6 class="mt-4 text-xl">Una simple app para buscar paises</h6>
+    </div>
+</template>
